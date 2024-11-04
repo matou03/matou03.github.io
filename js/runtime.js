6b1099d55b8b0518f75345a23aab1aff4dd252ec
m0_71695346 <m0_71695346@noreply.gitcode.com>
2024-11-04 20:41:00 +0800

    Site updated: 2024-11-04 20:40:58

--- js/runtime.js
+++ js/runtime.js
@@ -22,7 +22,7 @@ function createtime() {
         `<img class='boardsign' src='/img_1/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br>
         <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i>
         <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>` :
-        `<img class='boardsign' src='/img_1/F小屋-科研摸鱼中.svg' title='下班了就该开开心心地玩耍~'><br>
+        `<img class='boardsign' src='/img_1/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br>
         <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i>
         <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`;
     if (document.getElementById("workboard")) {
